93519c60aef8b97cdaa820d7834240e16f1cad50
Te0reTiK <te0retik314@yandex.ru>
2018-06-01 18:24:52 +0300

    env-vars for db-config

--- aviasales_hakaton/aviasales_hakaton/settings.py
+++ aviasales_hakaton/aviasales_hakaton/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'aviasales_hakaton.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DBHOST', ''),
+        'NAME': os.environ.get('DBNAME', ''),
+        'PORT': os.environ.get('DBPORT', ''),
+        'USER': os.environ.get('DBUSER', ''),
+        'PASSWORD': os.environ.get('DBPASSWORD', ''),
     }
 }
 
