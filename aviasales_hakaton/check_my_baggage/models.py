45111f93b8a56f7f82e37f6d1eacb054f7892b5c
Te0reTiK <te0retik314@yandex.ru>
2018-06-02 09:17:42 +0300

    model AirlineCompany - icon logo on_delete=SET_NULL

--- aviasales_hakaton/check_my_baggage/models.py
+++ aviasales_hakaton/check_my_baggage/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from django.db.models import SET_NULL
 
 from filer.fields.image import FilerImageField
 
@@ -9,8 +10,8 @@ class AirlineCompany(models.Model):
 
     name = models.CharField(verbose_name='Name', max_length=1024)
     code = models.CharField(verbose_name='IATA 2 chars code', max_length=2, db_index=True)
-    icon = FilerImageField(verbose_name='Logo icon 50x50', related_name='icon', blank=True, null=True)
-    logo = FilerImageField(verbose_name='Logo image [large]', related_name='logo', blank=True, null=True)
+    icon = FilerImageField(verbose_name='Logo icon 50x50', related_name='icon', blank=True, null=True, on_delete=SET_NULL)
+    logo = FilerImageField(verbose_name='Logo image [large]', related_name='logo', blank=True, null=True, on_delete=SET_NULL)
     description = models.TextField(verbose_name='Decription', blank=True, null=True)
     baggage_allowance_link = models.URLField(verbose_name='Baggage allowance link')
 
