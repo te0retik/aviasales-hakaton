43ba32ab85280505e501fa3c8de6415067cca86e
Te0reTiK <te0retik314@yandex.ru>
2018-06-02 11:08:20 +0300

    fix airline detail view route - now accept digits

--- aviasales_hakaton/check_my_baggage/urls.py
+++ aviasales_hakaton/check_my_baggage/urls.py
@@ -6,5 +6,5 @@ from check_my_baggage.views import AirlinesListView, AirlineDetailView
 
 urlpatterns = [
     url(r'^airlines/$', AirlinesListView.as_view()),
-    url(r'^airlines/(?P<code>[A-Z]{2})/$', AirlineDetailView.as_view()),
+    url(r'^airlines/(?P<code>[A-Z1-9]{2})/$', AirlineDetailView.as_view()),
 ]
