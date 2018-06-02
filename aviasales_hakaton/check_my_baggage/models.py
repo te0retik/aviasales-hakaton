4b8a5660f60c66a4f084ddf7ecdeedc0994bad4a
Te0reTiK <te0retik314@yandex.ru>
2018-06-02 08:11:40 +0300

    admin list airline companies show image and baggage_allowance_link

--- aviasales_hakaton/check_my_baggage/models.py
+++ aviasales_hakaton/check_my_baggage/models.py
@@ -5,6 +5,8 @@ from filer.fields.image import FilerImageField
 
 
 class AirlineCompany(models.Model):
+    BAGGAGE_ALLOWANCE_LINK_CROP_VIEW_LEN = 20
+
     name = models.CharField(verbose_name='Name', max_length=1024)
     code = models.CharField(verbose_name='IATA 2 chars code', max_length=2, db_index=True)
     icon = FilerImageField(verbose_name='Logo icon 50x50', related_name='icon', blank=True, null=True)
@@ -27,3 +29,29 @@ class AirlineCompany(models.Model):
         return 'AirlineCompany(%s, %s, %sx%sx%s, %s)' % (
             self.name, self.code, self.carryon_max_x, self.carryon_max_y,
             self.carryon_max_z, self.baggage_3dimensions)
+
+    def icon_img(self):
+        url = None
+        if self.icon:
+            url = self.icon.url
+            if self.icon.thumbnails:
+                # admin_clipboard_icon, admin_directory_listing_icon
+                # admin_sidebar_preview, admin_tiny_icon
+                url = self.icon.thumbnails.get('admin_tiny_icon')
+                if not url:
+                    url = self.icon.thumbnails.get('admin_directory_listing_icon')
+                if not url:
+                    url = self.icon.thumbnails.get('admin_sidebar_preview')
+
+        return '<img src="%s"/>' % url if url else ''
+    icon_img.short_description = 'icon'
+    icon_img.allow_tags = True
+
+    def baggage_allowance_link_href(self):
+        if len(self.baggage_allowance_link) > self.BAGGAGE_ALLOWANCE_LINK_CROP_VIEW_LEN:
+            link_cropped = self.baggage_allowance_link[:self.BAGGAGE_ALLOWANCE_LINK_CROP_VIEW_LEN] + '...'
+        else:
+            link_cropped = self.baggage_allowance_link
+        return '<a href="%s">%s</a>' % (self.baggage_allowance_link, link_cropped)
+    baggage_allowance_link_href.short_description = 'icon'
+    baggage_allowance_link_href.allow_tags = True
